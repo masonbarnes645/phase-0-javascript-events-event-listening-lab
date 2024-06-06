6f6f59458c195de7fc2f0a1393a9fa7ef545aa4f
Mason Barnes <barnesmason980@gmail.com>
2024-06-05 18:28:01 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+
+    const input = document.querySelector('#button');
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
+
 }
